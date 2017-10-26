532b115393af0c47d3479221be5308568a9d53d9
NIRANJAN DASAR <niranjan.dasar@gmail.com>
2017-10-26 10:15:44 +0530

    first commit

---
+++ src/main/java/com/cgi/netbanking/jenkins_Maven_NetBanking/App.java
@@ -0,0 +1,13 @@
+package com.cgi.netbanking.jenkins_Maven_NetBanking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
